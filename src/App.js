132a5fb2e2380716d76d01962307c8070c3ce05f
Aman-Raut <amanraut13@gmail.com>
2024-02-03 01:59:28 +0530

    modified:   db.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import {useEffect, useState} from 'react';
 function App() {
 
   const [data,setData] = useState([])
+  const[name,setName] = useState("")
+  const[email,setEmail] = useState("")
+  const[mobile,setMobile] = useState("")
+
+  function saveUser(){
+    console.warn({name,email,mobile})
+    let data={name,email,mobile}
+    fetch("http://localhost:4000/post",{
+      method:'POST',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application.json'
+      },
+      body:JSON.stringfy(data)
+    }).then((result)=>{
+      //console.warn("result",result
+      result.json().then((resp)=>{
+        console.log("resp")
+      })
+      })
+  }
 
   useEffect(()=>{
     fetch("http://localhost:4000/post").then((result)=>
@@ -16,7 +37,7 @@ function App() {
 },[])
 console.warn(data)
   return(
-    <div>
+    <div className='primary'>
        <h1>React API</h1>
         <table border="1px">
           <tr>
@@ -39,6 +60,12 @@ console.warn(data)
           )
         }
         </table>
+        <br />
+        <h2>Fill to edit.</h2>
+        <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}} placeholder='name' /><br />
+        <input type="text" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='email'/> <br />
+        <input type="text" name="mobile" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} placeholder='mobile'/>
+        <button onClick={saveUser} >Submit</button>
     </div>
    
   )
